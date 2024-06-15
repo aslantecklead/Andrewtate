5b9c8def8da85e8e0b7a654514b86aec563ba6b6
aslantecklead <97795768+aslantecklead@users.noreply.github.com>
2024-06-15 16:47:08 +0300

    Датагрид

--- Andrewtate/Windows/Users/UserWindow.xaml.cs
+++ Andrewtate/Windows/Users/UserWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Andrewtate.Windows.Users
 {
@@ -19,9 +20,40 @@ namespace Andrewtate.Windows.Users
     /// </summary>
     public partial class UserWindow : Window
     {
+        RailwaysEntities db = new RailwaysEntities();
         public UserWindow()
         {
             InitializeComponent();
+            Load_Data();
+        }
+
+        private void Load_Data()
+        {
+            var stations = db.Stantion.ToList();
+            dgStations.SelectedValuePath = "ID_Station";
+            dgStations.ItemsSource = stations;
+            dgStations.SelectionMode = DataGridSelectionMode.Single;
+
+            var trains = db.Train.ToList();
+            cbTrain.ItemsSource = trains;
+            cbTrain.SelectedIndex = 0;
+
+            cbTrain.SelectedValuePath = "ID";
+            cbTrain.DisplayMemberPath = "Name";
+
+            Count.Text = db.Train.Count().ToString();
+        }
+
+        private void dgStations_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgStations.SelectedItem != null)
+            {
+                Stantion selectedTrains = (Stantion)dgStations.SelectedItem;
+                tbName.Text = selectedTrains.Name;
+                tbSize.Text = selectedTrains.Size.ToString();
+                tbTown.Text = selectedTrains.Town;
+                cbTrain.SelectedItem = selectedTrains.Train;
+            }
         }
     }
 }
